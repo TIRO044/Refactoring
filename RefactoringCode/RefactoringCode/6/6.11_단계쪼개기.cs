f45669bdc61510e120a6c09d96eed4f0028fb5e7
junghojoon <junghojoon@studiobside.com>
2022-12-09 16:07:37 +0900

    [6.11 단계쪼개기] 예제 1

--- RefactoringCode/RefactoringCode/6/6.11_단계쪼개기.cs
+++ RefactoringCode/RefactoringCode/6/6.11_단계쪼개기.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RefactoringCode._6
+﻿namespace RefactoringCode._6
 {
-    internal class _6
+    namespace RefactoringCode._6._Ex1
     {
+        internal class _6_11_Before
+        {
+            Dictionary<string, int> priceList = new Dictionary<string, int>();
+
+            public int Before(string orderString)
+            {
+                var orderData = orderString.Split("s+");
+                var productPrice = priceList[orderData[0].Split("-")[1]];
+                var orderPrice = int.Parse(orderData[0]) * productPrice;
+                return orderPrice;
+            }
+        }
+
+        // ---------------------- ---------------------- ---------------------- ---------------------- ---------------------- ----------------------
+        // ---------------------- ---------------------- ---------------------- ---------------------- ---------------------- ----------------------
+        // ---------------------- ---------------------- ---------------------- ---------------------- ---------------------- ----------------------
+
+        internal class _6_11_After
+        {
+            class OrderDataClass
+            {
+                public string ProductID;
+                public int Quantity;
+            }
+
+            Dictionary<string, int> priceList = new Dictionary<string, int>();
+
+            public int After(string orderString)
+            {
+                var orderDataClass = ParseOrder(orderString);
+                return Price(orderDataClass, priceList);
+
+                OrderDataClass ParseOrder(string aString)
+                {
+                    var values = aString.Split("s+");
+                    return new OrderDataClass { ProductID = values[1], Quantity = int.Parse(values[0]) };
+                }
+
+                int Price(OrderDataClass order, Dictionary<string, int> priceOrder)
+                {
+                    return order.Quantity * priceOrder[order.ProductID];
+                }
+            }
+        }
     }
 }
