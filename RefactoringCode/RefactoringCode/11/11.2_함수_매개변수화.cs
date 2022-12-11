285cabd5c6f717a1e0182df0c2ec9e6db11a9cd1
emddldans <wjdqkddl044@naver.com>
2022-12-11 18:16:35 +0900

    [11.2 함수 매개변수화]
     - 비슷한 로직의 함수가 여러 개 있을 경우 합친다.
     - 조건이 하나 있는데, 로직은 같지만 함수에 [상수]를 박아 하드 코딩한 경우
     - 요럴 경우엔 로직을 이해한 뒤 하나의 함수로 변경해준다.

---
+++ RefactoringCode/RefactoringCode/11/11.2_함수_매개변수화.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace RefactoringCode._11
+{
+    public class _11_2_Before
+    {
+        public float BaseCharge(float usage)
+        {
+            if (usage < 0) return 0;
+            float amount =
+                BottomBand(usage) * 0.3f
+                + MiddleBand(usage) * 0.5f
+                + TopBand(usage) * 0.7f;
+
+            return amount;
+        }
+
+        public float BottomBand(float usage)
+        {
+            return Math.Min(usage, 100);
+        }
+
+        public float MiddleBand(float usage)
+        {
+            return usage > 100 ? Math.Min(usage, 200) - 100 : 0;
+        }
+
+        public float TopBand(float usage)
+        {
+            return usage > 200 ? usage - 200 : 0;
+        }
+    }
+
+    public class _11_2_After
+    {
+        public float BaseCharge(float usage)
+        {
+            if (usage < 0) return 0;
+            float amount =
+                WithinBand(usage, 0, 100) * 0.3f +
+                WithinBand(usage, 100, 200) * 0.5f +
+                WithinBand(usage, 200, float.MaxValue) * 0.7f;
+
+            return amount;
+        }
+
+        public float WithinBand(float usage, float bottom, float top)
+        {
+            return usage > bottom ? Math.Min(usage, top) - bottom : 0;
+        }
+    }
+}
+
